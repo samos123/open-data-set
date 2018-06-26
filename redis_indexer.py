b8d151c24bc1f2b44f673b6c36ae5d523f814ec1
Sam Stoelinga <sammiestoel@gmail.com>
2018-06-25 21:05:09 -0700

    Add redis_indexer

---
+++ redis_indexer.py
@@ -0,0 +1,22 @@
+import os
+import json
+import redis
+from walrus import Database
+
+redis_host = os.environ.get('REDISHOST', 'localhost')
+redis_port = int(os.environ.get('REDISPORT', 6379))
+#redis_client = redis.StrictRedis(host=redis_host, port=redis_port)
+walrus_db = Database(host=redis_host, port=redis_port)
+ac = walrus_db.autocomplete()
+
+def read_json_file(path):
+    return json.loads(open(path).read())
+
+def populate_index():
+    products = read_json_file("products.json")
+    for product in products:
+        ac.store(obj_id=product['sku'], title=product['name'])
+
+if __name__ == "__main__":
+    populate_index()
+    print(ac.search("Bat"))
